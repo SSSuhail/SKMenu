7c1a731c82edd519f49632f6ffd71a18d804fe48
Suhail Shabir <suhail.shabir@dltlabs.io>
2019-04-30 12:32:25 +0530

    Initial Commit

---
+++ SideMenu/SideMenu.h
@@ -0,0 +1,19 @@
+//
+//  SideMenu.h
+//  SideMenu
+//
+//  Created by Suhail.Shabir on 30/04/19.
+//  Copyright © 2019 DLT Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SideMenu.
+FOUNDATION_EXPORT double SideMenuVersionNumber;
+
+//! Project version string for SideMenu.
+FOUNDATION_EXPORT const unsigned char SideMenuVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SideMenu/PublicHeader.h>
+
+
